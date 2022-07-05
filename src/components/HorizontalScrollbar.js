13eabeb9bc30484fe62ba74aa3d5da1683c98b70
Shail_Office <shailshah.officework@gmail.com>
2022-07-05 18:16:26 +0530

    completed gym project from Javascript Mastery --office

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,10 +1,36 @@
-import React from "react";
-import BodyPart from "./BodyPart";
-import { Box } from "@mui/material";
+import React, { useContext } from "react";
+import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
+import { Box, Typography } from "@mui/material";
 
-const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
+import BodyPart from "./BodyPart";
+import ExerciseCard from "./ExerciseCard";
+
+import RightArrowIcon from "../assets/icons/right-arrow.png";
+import LeftArrowIcon from "../assets/icons/left-arrow.png";
+
+const LeftArrow = () => {
+  const { scrollPrev } = useContext(VisibilityContext);
+
   return (
-    <div>
+    <Typography onClick={() => scrollPrev()} className="right-arrow">
+      <img src={LeftArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
+
+const RightArrow = () => {
+  const { scrollNext } = useContext(VisibilityContext);
+
+  return (
+    <Typography onClick={() => scrollNext()} className="left-arrow">
+      <img src={RightArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
+
+const HorizontalScrollbar = ({ data, bodyPart, setBodyPart, isBodyParts }) => {
+  return (
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
       {data.map((item) => (
         <Box
           key={item.id || item}
@@ -12,13 +38,18 @@ const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
           title={item.id || item}
           m="0 40px"
         >
-          <BodyPart 
-          item={item} 
-          bodyPart={bodyPart} 
-          setBodyPart={setBodyPart} />
+          {isBodyParts ? (
+            <BodyPart
+              item={item}
+              bodyPart={bodyPart}
+              setBodyPart={setBodyPart}
+            />
+          ) : (
+            <ExerciseCard exercise={item} />
+          )}
         </Box>
       ))}
-    </div>
+    </ScrollMenu>
   );
 };
 
